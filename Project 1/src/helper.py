d891ff8009ef91995fa28a67f6c93992a2b53e19
Lehhanh <lethihonghanh10804@gmail.com>
2024-07-22 22:24:07 +0700

    get path from explored set - BFS, UCS, GBFS, Astar

---
+++ Project 1/src/helper.py
@@ -0,0 +1,30 @@
+from Node import *
+
+# Read input
+def readInput(filepath):
+    map = []
+    f = open(filepath)
+    row, col = f.readline().split()
+    try:
+        row = int(row)
+        col = int(col)
+        for i in range (0, row):
+            line = f.readline()
+            newRow = line.split()
+            map.append(newRow)
+    except:
+        print('The number of row or column is invalid')
+    return map
+
+# Getting path from explored set
+def reconstructPath(exploredSet, startPos, goalPos, isGinE):
+    path = []
+    i = len(exploredSet) - 1
+    if not isGinE:
+        path.append(goalPos)
+    while True:
+        path = [exploredSet[i].position] + path
+        if exploredSet[i].position == startPos:
+            break
+        i = [node.position for node in exploredSet].index(exploredSet[i].parent)
+    return path
---
+++ Project 1/src/main.py
@@ -0,0 +1,7 @@
+from helper import *
+from level_1 import *
+
+
+map = readInput('..\\input\\input1_level1.txt')
+algorithm = input('Enter algorithm: ')
+pathfindingLevel_1(algorithm, map)
