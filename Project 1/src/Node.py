d8cdbcd2b5a702873bb6ec8cada38b800ac33493
TuMinz <129361284+Tuminz@users.noreply.github.com>
2024-07-23 22:11:02 +0700

    Update Node.py

--- Project 1/src/Node.py
+++ Project 1/src/Node.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Node:
     def __init__(self, position, parent, g_cost, h_cost):
         self.position = position
@@ -10,10 +11,11 @@ class Node:
         return self.f_cost < other.f_cost
 
     def getNeighborPos(self, map):
+        map = np.array(map)
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] #Right, Down, Left, Up
         neighbors = []
         for d in directions:
             newPos = tuple([a+b for a, b in zip(self.position, d)])
-            if 0 <= newPos[0] < len(map) and 0 <= newPos[1] < len(map[0]) and map[newPos] != '-1':
+            if (0 <= newPos[0] < len(map) and 0 <= newPos[1] < len(map[0]) and map[newPos] != '-1'):
                 neighbors.append(newPos)
         return neighbors
