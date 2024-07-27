663c3012d2d6143b625265be52a1a5340ed4c335
TuMinz <129361284+Tuminz@users.noreply.github.com>
2024-07-27 17:18:56 +0700

    Update level_1.py

--- Project 1/src/level_1.py
+++ Project 1/src/level_1.py
@@ -1,6 +1,6 @@
 import numpy as np
 from Node import *
-from helper import *
+from helper import*
 
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] #Right, Down, Left, Up
 
@@ -10,7 +10,7 @@ def heuristic(currentPos, goalPos):
 
 # Breadth-first search
 def BFS(map, startPos, goalPos):
-    frontier = [Node(startPos, None, 0, 0)]
+    frontier = [Node(startPos, None, 0)]
     exploredSet = []
     while len(frontier) > 0:
         currentNode = frontier.pop(0)
@@ -20,13 +20,13 @@ def BFS(map, startPos, goalPos):
             if neighbor == goalPos:
                 return True, frontier, exploredSet
             if neighbor not in [n.position for n in frontier] and neighbor not in [n.position for n in exploredSet]:
-                frontier.append(Node(neighbor, currentNode.position, currentNode.g_cost + 1, 0))
+                frontier.append(Node(neighbor, currentNode.position, currentNode.g_cost + 1))
     return False, frontier, exploredSet
 
 
 # Uniform-cost search
 def UCS(map, startPos, goalPos):
-    frontier = [Node(startPos, None, 0, 0)]
+    frontier = [Node(startPos, None, 0)]
     exploredSet = []
     while len(frontier) > 0:
         frontier.sort()
@@ -38,11 +38,11 @@ def UCS(map, startPos, goalPos):
         for neighbor in neighborPos:
             if neighbor not in [n.position for n in exploredSet]:
                 if neighbor not in [n.position for n in frontier]:
-                    frontier.append(Node(neighbor, currentNode.position, currentNode.g_cost + 1, 0))
+                    frontier.append(Node(neighbor, currentNode.position, currentNode.g_cost + 1))
                 else:
                     index = [n.position for n in frontier].index(neighbor)
                     if currentNode.g_cost + 1 < frontier[index].g_cost:
-                        frontier[index].updateCost(currentNode.g_cost + 1, 0)
+                        frontier[index].updateCost(currentNode.g_cost + 1)
                         frontier[index].parent = currentNode.position
     return False, frontier, exploredSet
 
@@ -52,7 +52,8 @@ def GBFS(map, startPos, goalPos):
     frontier = [Node(startPos, None, 0, heuristic(startPos, goalPos))]
     exploredSet = []
     while len(frontier) > 0:
-        currentNode = frontier.pop([n.h_cost for n in frontier].index(min([n.h_cost for n in frontier])))
+        frontier.sort(key=lambda n: n.h_cost)
+        currentNode = frontier.pop(0)
         exploredSet.append(currentNode)
         neighborPos = currentNode.getNeighborPos(map)
         for neighbor in neighborPos:
@@ -87,48 +88,59 @@ def Astar(map, startPos, goalPos):
 
 
 # Depth-first search
-def DFS(map, startPos, goalPos, path):
-    frontier = [Node(startPos, None, 0, 0)]
+def DFS(map, startPos, goalPos):
+    frontier = [Node(startPos, None, 0)]
     exploredSet = []
     while len(frontier) > 0:
         currentNode = frontier.pop()
         if currentNode.position not in [n.position for n in exploredSet]:
             exploredSet.append(currentNode)
             if currentNode.position == goalPos:
-                return True
-            for d in directions:
-                newPos = tuple([a+b for a, b in zip(currentNode.position, d)])
-                if 0 <= newPos[0] < len(map[0]) and 0 <= newPos[1] < len(map[1]) and newPos not in [n.position for n in exploredSet]:
-                    frontier.append(Node(newPos, currentNode.position, 0, 0))
+                return True, frontier, exploredSet
+            neighborPos = currentNode.getNeighborPos(map)
+            for neighbor in neighborPos:
+                frontier.append(Node(neighbor, currentNode.position, currentNode.g_cost + 1))
+    return False, frontier, exploredSet
 
 
-def pathfindingLevel_1(algorithm, map):
-    startPos = [(index, row.index('S')) for index, row in enumerate(map) if 'S' in row][0]
-    goalPos = [(index, row.index('G')) for index, row in enumerate(map) if 'G' in row][0]
-    map = np.array(map)
-    path = []
-    if algorithm == 'BFS':
-        isSuccess, frontier, exploredSet = BFS(map, startPos, goalPos)
-        path = reconstructPath(exploredSet, startPos, goalPos, False)
-    elif algorithm == 'UCS':
-        isSuccess, frontier, exploredSet = UCS(map, startPos, goalPos)
-        path = reconstructPath(exploredSet, startPos, goalPos, True)
-    elif algorithm == 'GBFS':
-        isSuccess, frontier, exploredSet = GBFS(map, startPos, goalPos)
-        path = reconstructPath(exploredSet, startPos, goalPos, False)
-    elif algorithm == 'Astar':
-        isSuccess, frontier, exploredSet = Astar(map, startPos, goalPos)
-        path = reconstructPath(exploredSet, startPos, goalPos, True)
-    elif algorithm == 'DFS':
-        isSuccess, frontier, exploredSet = DFS(map, startPos, goalPos)
-    else:
-        print('Algorithm is invaid')
-        return -1
+
+def pathFinding_level1(map):
+    startPos = findPosition(map, 'S')[0]
+    goalPos = findPosition(map, 'G')[0]
+    pathList = []
+    # BFS
+    isSuccess, frontier, exploredSet = BFS(map, startPos, goalPos)
     if isSuccess:
-        print('Frontier: ',[n.position for n in frontier])
-        print('Explored Set: ', [n.position for n in exploredSet])
-        print('Parent Set: ', [n.parent for n in exploredSet])
-        print('Path:', path)
-        print('Path cost: ', len(path) - 1)
+        path = reconstructPath(exploredSet, startPos, goalPos, False)
     else:
-        print('Cannat find any paths to destination') 
+        path = []
+    pathList.append(path)
+    # DFS
+    isSuccess, frontier, exploredSet = DFS(map, startPos, goalPos)
+    if isSuccess:
+        path = reconstructPath(exploredSet, startPos, goalPos, True)
+    else:
+        path = []
+    pathList.append(path)
+    # UCS
+    isSuccess, frontier, exploredSet = UCS(map, startPos, goalPos)
+    if isSuccess:
+        path = reconstructPath(exploredSet, startPos, goalPos, True)
+    else:
+        path = []
+    pathList.append(path)
+    # A*
+    isSuccess, frontier, exploredSet = Astar(map, startPos, goalPos)
+    if isSuccess:
+        path = reconstructPath(exploredSet, startPos, goalPos, True)
+    else:
+        path = []
+    pathList.append(path)
+    # GBFS
+    isSuccess, frontier, exploredSet = GBFS(map, startPos, goalPos)
+    if isSuccess:
+        path = reconstructPath(exploredSet, startPos, goalPos, False)
+    else:
+        path = []
+    pathList.append(path)
+    return pathList
